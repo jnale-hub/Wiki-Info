6b86442019ff49b8846cd5d0da961c56287dc501
John Mark Delima <delimajohnmark14@gmail.com>
2023-01-03 11:35:29 +0800

    Added modificition on error messsage

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,7 +4,13 @@ import random
 
 from . import util
 
+
 def convert_md_to_html(title):
+    """
+    Getting the content from the entry and convert it to html 
+    content. If there is no content, return None. This function 
+    is used to convert entries using the title.
+    """
     content = util.get_entry(title)
     markdowner = Markdown()
     if content == None:
@@ -12,12 +18,23 @@ def convert_md_to_html(title):
     else:
         return markdowner.convert(content)
 
+
 def index(request):
+    """
+    Render the index html and all the entries using 
+    the list_entries function from util.
+    """
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
 def entry(request, title):
+    """
+    Convert the entry. If entry does not exist, deliver 
+    an error message. Else, render the title and the converted 
+    content on the entry html.
+    """
     html_content = convert_md_to_html(title)
     if html_content == None:
         return render(request, "encyclopedia/error.html", {
@@ -29,37 +46,49 @@ def entry(request, title):
             "content": html_content,
         })
 
+
 def search(request):
     if request.method == "POST":
+        # Get the search entry using the q value
         entry_search = request.POST['q']
         html_content = convert_md_to_html(entry_search)
+        # If there is an entry, render
         if html_content is not None:
             return render(request, "encyclopedia/entry.html", {
                 "title": entry_search,
                 "content": html_content
             })
+        # If there is None, make a recommendation using the letter in entries
         else:
             allEntries = util.list_entries()
             recommendation = []
             for entry in allEntries:
                 if entry_search.lower() in entry.lower():
                     recommendation.append(entry)
+                else:
+                    return render(request, "encyclopedia/error.html", {
+                        "message": "There is no result. <a href='../new/'>Try to add an entry</a>"
+                    })
             return render(request, "encyclopedia/search.html", {
                 "recommendation": recommendation
             })
 
+
+
+
 def new_page(request):
+    # Function to create a new page
     if request.method == "GET":
         return render(request, "encyclopedia/new.html")
     else:
         title = request.POST['title']
         content = request.POST['content']
         titleExist = util.get_entry(title)
+        # Deliver an error message if there is an existing entry
         if titleExist is not None:
             return render(request, "encyclopedia/error.html", {
                 "message": "Entry page already exist"
             })
-        
         else:
             util.save_entry(title, content)
             html_content = convert_md_to_html(title)
@@ -68,14 +97,16 @@ def new_page(request):
                 "content": html_content
             })
 
+
 def edit(request):
     if request.method == "POST":
         title = request.POST['entry_title']
         content = util.get_entry(title)
         return render(request, "encyclopedia/edit.html", {
-                "title": title,
-                "content": content
-            })
+            "title": title,
+            "content": content
+        })
+
 
 def save_edit(request):
     if request.method == "POST":
@@ -87,6 +118,7 @@ def save_edit(request):
             "title": title,
             "content": html_content
         })
+
 
 def rand(request):
     allEntries = util.list_entries()
