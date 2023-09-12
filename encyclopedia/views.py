04475e5e065834765c2c10f9d5ad873b876e4df2
John Mark Delima <delimajohnmark14@gmail.com>
2023-09-12 16:53:55 +0800

    Troubleshoot to unquote the entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,71 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from markdown2 import Markdown
 import random
 
 from . import util
 
-
 def convert_md_to_html(title):
     """
-    Getting the content from the entry and convert it to html 
-    content. If there is no content, return None. This function 
-    is used to convert entries using the title.
+    Convert Markdown content to HTML.
     """
     content = util.get_entry(title)
     markdowner = Markdown()
-    if content == None:
+    if content is None:
         return None
-    else:
-        return markdowner.convert(content)
-
+    return markdowner.convert(content)
 
 def index(request):
     """
-    Render the index html and all the entries using 
-    the list_entries function from util.
+    Render the index HTML and display a list of entries.
     """
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(),
         'active_page': 'home'
     })
 
-
 def entry(request, title):
     """
-    Convert the entry. If entry does not exist, deliver 
-    an error message. Else, render the title and the converted 
-    content on the entry html.
+    Render an entry by converting its Markdown content to HTML.
     """
+    # Try to find an exact match for the provided title
     html_content = convert_md_to_html(title)
-    if html_content == None:
+
+    if html_content is None:
+        # If there's no exact match, attempt to find a case-insensitive match
+        all_entries = util.list_entries()
+        matching_entries = [entry for entry in all_entries if title.lower() == entry.lower()]
+
+        if matching_entries:
+            # If a case-insensitive match is found, redirect to the correct entry
+            return redirect('entry', title=matching_entries[0])
+
+        # If no matches are found, display an error message
         return render(request, "encyclopedia/error.html", {
             "message": "This entry does not exist."
         })
-    else:
-        return render(request, "encyclopedia/entry.html", {
-            "title": title,
-            "content": html_content,
-        })
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": html_content,
+    })
 
 
 def search(request):
     if request.method == "POST":
-        # Get the search entry using the q value
-        entry_search = request.POST['q']
+        entry_search = request.POST.get('q', '')
         html_content = convert_md_to_html(entry_search)
-        # If there is an entry, render
         if html_content is not None:
             return render(request, "encyclopedia/entry.html", {
                 "title": entry_search,
                 "content": html_content
             })
-        # If there is None, make a recommendation using the letter in entries
         else:
             allEntries = util.list_entries()
-            recommendation = []
-            for entry in allEntries: 
-                if entry_search.lower() in entry.lower():
-                    recommendation.append(entry)
+            recommendation = [entry for entry in allEntries if entry_search.lower() in entry.lower()]
             if not recommendation:
                 return render(request, "encyclopedia/error.html", {
                     "entry_search": entry_search,
@@ -76,29 +72,25 @@ def search(request):
                     "recommendation": recommendation
                 })
 
-
-
 def new_page(request):
-    # Function to create a new page
     if request.method == "GET":
         return render(request, "encyclopedia/new.html", {'active_page': 'new_page'})
-    else:
-        title = request.POST['title']
-        content = request.POST['content']
-        titleExist = util.get_entry(title)
-        # Deliver an error message if there is an existing entry
-        if titleExist is not None:
+    elif request.method == "POST":
+        title = request.POST.get('title', '')
+        content = request.POST.get('content', '')
+        
+        if not title or not content:
             return render(request, "encyclopedia/error.html", {
-                "message": "Entry page already exist."
-            })
-        else:
-            util.save_entry(title, content)
-            html_content = convert_md_to_html(title)
-            return render(request, "encyclopedia/entry.html", {
-                "title": title,
-                "content": html_content,
+                "message": "Title and content are required."
             })
 
+        if util.get_entry(title) is not None:
+            return render(request, "encyclopedia/error.html", {
+                "message": "Entry already exists."
+            })
+
+        util.save_entry(title, content)
+        return redirect('entry', title=title)  # Redirect to the newly created entry
 
 def edit(request):
     if request.method == "POST":
